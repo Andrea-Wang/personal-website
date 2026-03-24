ae96da403226d66c1a5a082816929ef72d0c68fc
chen <chen@chendeMacBook-Air.local>
2026-03-24 12:42:15 +0100

    Switch to html routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://wangchenlab.com',
-  trailingSlash: 'always',
+  trailingSlash: 'never',
+  build: {
+    format: 'file',
+  },
 });
